76a6918b84e402762d76830a476cf4b4542cf0dc
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2024-04-02 20:50:53 +0200

    String dilemma JS

---
+++ Tema_2/00_Resources/Codigo de ejemplo/00_Exec_Dilemma_String_Type.js
@@ -0,0 +1,8 @@
+// ¿Qué ocurre con los Strings en Javascript?
+// ¿Qué ocurre si ejecutamos el siguiente código?
+
+let cadena = "Hello Dilemma";
+console.log(typeof cadena); // ¿Qué tipo de dato es?
+console.log(typeof new String("hola")); // ¿Qué tipo de dato es?
+console.log(cadena instanceof String); // ¿Es una instancia de String?
+console.log(new String("hola") instanceof String); // ¿Qué tipo de dato es?
